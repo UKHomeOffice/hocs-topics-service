e850774ff3e9428e40a5adad892dffba9cb554fa
Liam MacPherson <liam.macpherson@digital.homeoffice.gov.uk>
2022-12-20 13:25:46 +0000

    HOCS-5924: add baseline schema
    Add the tables for the topics schema e.g. `topic`, `parent_topic`, and `topic_link` (renamed from `team_link`).

---
+++ src/main/resources/db/schema/V2__AddTopicSchema.sql
@@ -0,0 +1,20 @@
+create table topic
+(
+    id                bigserial
+        constraint topic_pkey
+            primary key,
+    display_name      text                 not null,
+    uuid              uuid                 not null
+        constraint topic_uuid_idempotent
+            unique,
+    parent_topic_uuid uuid                 not null,
+    active            boolean default true not null,
+    constraint topic_name_idempotent
+        unique (display_name, parent_topic_uuid)
+);
+
+create index idx_topic_uuid
+    on topic (uuid);
+
+create index idx_topic_all
+    on topic (display_name, uuid, parent_topic_uuid);
---
+++ src/main/resources/db/schema/V3__AddTopicLInkSchema.sql
@@ -0,0 +1,16 @@
+create table topic_link
+(
+    id                    bigserial
+        constraint topic_link_pkey
+            primary key,
+    case_type             text        not null,
+    stage_type            text        not null,
+    link_value            varchar(80) not null,
+    link_type             text        not null,
+    responsible_team_uuid uuid        not null,
+    constraint topic_link_link_uuid_case_type_stage_type_link_type_key
+        unique (link_value, case_type, stage_type, link_type)
+);
+
+create index idx_topic_link_case_stage_link
+    on topic_link (case_type, stage_type, link_value);
